e5bfba14904f6a011986d6fd661a9a34dc2d1104
crispymccoy145 <samspam19941@gmail.com>
2021-04-16 15:12:51 +0100

    cpu added, code refactored. Now to css/decorate game

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template, request, redirect, url_for
 from models.players.player import Player
 from models.games.game import Game
-from models.games.play import game_1, set_player_1_attribute, set_player_2_attribute
+from models.games.play import game_1
 
 
 @app.route('/')
@@ -13,7 +13,7 @@ def index():
 def pvp1():
     if request.method == 'POST':
         player_1_move = request.form.get('move')
-        set_player_1_attribute(game_1, player_1_move)
+        game_1.set_player_1_attribute(player_1_move)
         return redirect(url_for('pvp2'))
     return render_template('player-1.html')
 
@@ -21,15 +21,23 @@ def pvp1():
 def pvp2():
     if request.method == 'POST':
         player_2_move = request.form.get('move')
-        set_player_2_attribute(game_1, player_2_move)
+        game_1.set_player_2_attribute(player_2_move)
         return redirect(url_for('pvpwinner'))
     return render_template('player-2.html')
 
 @app.route('/pvpwinner', methods=['POST', 'GET'])
 def pvpwinner():
-    return game_1.winner()
+    result = game_1.winner()
+    player_1_input = game_1.player_1_object.move
+    player_2_input = game_1.player_2_object.move
+    return render_template('winner.html', result=result, player_1_input=player_1_input, player_2_input=player_2_input)
 
 @app.route('/pvc1', methods=['POST', 'GET'])
 def pvc1():
+    if request.method == 'POST':
+        player_1_move = request.form.get('move')
+        game_1.set_player_1_attribute(player_1_move)
+        game_1.computer_move_select()
+        return redirect(url_for('pvpwinner'))
     return render_template('cpu-1.html')
 
--- models/games/game.py
+++ models/games/game.py
@@ -1,12 +1,20 @@
-
+from models.players.player import Player
+import random
 
 class Game:
 
-    def __init__(self, player_1_object, player_2_object):
+    def __init__(self, player_1_object, player_2_object= None):
 
         self.player_1_object = player_1_object
         self.player_2_object = player_2_object
         self.move_list = ['rock', 'paper', 'scissors']
+        
+    
+    def set_player_1_attribute(self, attribute_to_set):
+        self.player_1_object.move = attribute_to_set
+
+    def set_player_2_attribute(self, attribute_to_set):
+        self.player_2_object.move = attribute_to_set
     
     def winner(self):
         if self.player_1_object.move == 'rock':
@@ -30,3 +38,8 @@ class Game:
                 return "Player 1 wins"
             else:
                 return "It's a draw"
+            
+    def computer_move_select(self):
+        cpu_move= random.randint(0,2)
+        self.player_2_object = Player()
+        self.player_2_object.move = self.move_list[cpu_move]
